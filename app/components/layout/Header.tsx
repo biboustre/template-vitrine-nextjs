360a05389da416ded91541a665442e4065de8eb4
Habib <biboustre@hotmail.com>
2026-03-17 14:20:10 +0100

    maj

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -9,17 +9,6 @@ export default function Header() {
   return (
     <header className="w-full fixed z-20 ">
       {/* <AlertBanner /> */}
-      {/* <nav
-        aria-label="Navigation principale"
-        className="relative h-[80px] flex items-center justify-between w-full bg-primary backdrop-blur-md shadow z-50 px-4 md:px-6"
-      >
-        <Logo title="Mon Site" />
-        <Navbar />
-        <section className="flex items-center gap-2">
-          <DarkModeToggle />
-          <HamburgerMenu />
-        </section>
-      </nav> */}
       <MainMenu />
     </header>
   );
--- app/data/testimonials.ts
+++ app/data/testimonials.ts
@@ -2,19 +2,19 @@ export const testimonials = [
   {
     quote:
       "Travail de terrassement impeccable pour la construction de notre maison. Terrain parfaitement nivelé, chantier propre et délais respectés. Je recommande vivement !",
-    name: "Jean Dupont",
-    company: "Particulier à Saint-Étienne",
+    // name: "Jean Dupont",
+    // company: "Particulier à Saint-Étienne",
   },
   {
     quote:
       "Entreprise sérieuse pour l’aménagement de notre parking et l’installation du drainage. Équipe professionnelle, à l’écoute et matériel moderne.",
-    name: "Claire Leroy",
-    company: "Gérante, Leroy Transports",
+    // name: "Claire Leroy",
+    // company: "Gérante, Leroy Transports",
   },
   {
     quote:
       "Nous avons fait appel à eux pour la pose de notre fosse septique et le terrassement du jardin. Résultat parfait, conseils avisés et prix compétitifs.",
-    name: "Marc Petit",
-    company: "Propriétaire à Montbrison",
+    // name: "Marc Petit",
+    // company: "Propriétaire à Montbrison",
   },
 ];
