813b222cb6e2d9eeef6c47f0deb89b103d16ce9a
Habib <biboustre@hotmail.com>
2026-04-03 00:33:05 +0200

    maj

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./styles/globals.css";
@@ -214,6 +215,7 @@ export default function RootLayout({
           </main>
           <Footer />
           <Analytics />
+          <SpeedInsights />
         </DarkModeProvider>
       </body>
     </html>
